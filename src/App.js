85e8ab5ea9009c6014d2e7da74bb76de236725e7
ErkSt <erik_steg@yahoo.com>
2025-07-08 08:30:31 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+
+
+class ReactButton extends Component{
+  handleClick(event){
+
+    //Deprecated documentation: https://legacy.reactjs.org/docs/events.html
+
+    //Evento Sintetico
+    console.log(event);
+
+    //Evento Nativo del navegador
+    console.log(event.nativeEvent);
+
+  }
+  
+  render(){
+    return <button onClick={ this.handleClick }> Button Text </button>
+  }
+}
+
+class PointerCoordsVisualizer extends Component {
+  constructor(){
+    super();
+    this.state = { mouseX: 0, mouseY:0 }
+    //Enlazar evento
+    //this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+
+  //Enlazar evento
+  handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+
+    this.setState({
+      mouseX: clientX,
+      mouseY: clientY
+    })
+  }
+
+  render(){
+    return <div
+              onMouseMove={ this.handleMouseMove } 
+              style={{ border: '1px solid #000', marginTop: 10, padding: 10 }}>
+              
+              <p> {this.state.mouseX }, {this.state.mouseY }  </p>  
+            
+            </div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactButton />
+      <PointerCoordsVisualizer />
     </div>
   );
 }
